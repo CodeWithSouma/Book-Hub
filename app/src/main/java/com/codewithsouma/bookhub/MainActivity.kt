2cb95c67d94426572bc3102c0bbb62f24c59b2ce
Souma <souma.hitech@gmail.com>
2021-04-01 02:30:54 +0530

    refactor code

--- app/src/main/java/com/codewithsouma/bookhub/MainActivity.kt
+++ app/src/main/java/com/codewithsouma/bookhub/MainActivity.kt
@@ -96,9 +96,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun startDashboardFragment() {
         startFragment("Dashboard", DashboardFragment())
-//        val currentItem = navigationView.menu.getItem(0)
-//        currentItem.isChecked = true
-//        previousMenuItem = currentItem
         navigationView.setCheckedItem(R.id.dashboard)
     }
 
